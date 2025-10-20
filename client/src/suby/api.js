f66a60ea4e6fa02b499adf03b3aebd11c6e4462a
Avala vinay <avalavinay4@gmail.com>
2025-10-20 16:45:09 +0530

    Update API_URL to new backend endpoint

--- client/src/suby/api.js
+++ client/src/suby/api.js
@@ -1,3 +1,3 @@
 //export const API_URL = "http://localhost:5000"
 
-export const API_URL = "https://backend-nodejs-suby-1-54sr.onrender.com"
+export const API_URL = "https://backend-nodejs-suby-2-rts0.onrender.com"
