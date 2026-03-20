8d53df97c22fbf970fe78d9ebf8ad7e0f1e362f2
Avala vinay <avalavinay4@gmail.com>
2026-03-20 20:54:47 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import LandingPage from './suby/pages/LandingPage'
-import { Routes, Route } from 'react-router-dom'
+// client/src/App.jsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './suby/pages/LandingPage';
+import ProductMenu from './suby/components/ProductMenu';
+import { AppProvider } from './suby/AppContext';
 
-import './App.css'
-import ProductMenu from './suby/components/ProductMenu'
+const App = () => (
+  <AppProvider>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/products/:firmId/:firmName" element={<ProductMenu />} />
+    </Routes>
+  </AppProvider>
+);
 
-const App = () => {
-  return (
-    <div>
-      <Routes>
-          <Route path='/' element = { <LandingPage />} />
-          <Route path='/products/:firmId/:firmName' element = {<ProductMenu />} />
-      </Routes>
-    
-    </div>
-  )
-}
-
-export default App
+export default App;
